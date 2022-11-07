38221f24f79a2a97df37511c9eebe713e3410a5c
24HourDeveloper <latoya30310@gmail.com>
2022-11-07 16:03:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:3001')
+    .then((data)=> {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("error", error);
+    })
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Family Reunion</h1>
+    
     </div>
   );
 }
